5201f9131dc5f481b34ab51aae3ef8523ac9ec12
Edison Li <edison4354@gmail.com>
2020-12-29 22:11:20 -0800

    Making the front-end of login page

---
+++ src/lib/components/Button.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Text, View, TouchableOpacity, Dimensions, StyleSheet } from 'react-native'
+
+
+const width = Dimensions.get('window').width
+
+
+const Button = ({ text }) => {
+  return (
+    <TouchableOpacity>
+      <View style={styles.btnContainerStyle}>
+        <Text style={styles.btnTextStyle}> {text} </Text>
+      </View>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  btnContainerStyle: {
+    backgroundColor: 'transparent',
+    paddingVertical: 8,
+    width: width / 2,
+    borderRadius: 4,
+    borderWidth: 2,
+    borderColor: '#C7C3FB',
+    margin: 30,
+  },
+  btnTextStyle: {
+    color: '#6558F6',
+    fontSize: 16,
+    textAlign: 'center',
+  }
+})
+
+export default Button;
---
+++ src/screens/login/LoginScreen.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, TextInput, View, Dimensions } from 'react-native';
+import Button from 'lib/components/Button.js';
+
+const width = Dimensions.get('window').width
+
+export default class Login extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Login to golden slumbr</Text>
+        <TextInput style={styles.input} placeholder={'username'}/>
+        <TextInput style={styles.input} placeholder={'password'}/>
+        <Button
+          text="Next"
+          onPress={() => Alert.alert('Simple Button pressed')}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ffffff',
+  },
+  welcome: {
+    fontSize: 18,
+    textAlign: 'center',
+    margin: 30,
+
+  },
+  input: {
+    height: 40,
+    width: width / 2,
+    paddingLeft: 10,
+    margin: 15,
+    borderColor: '#AEBAC6',
+    borderWidth: 2,
+    borderRadius: 4,
+  },
+  next: {
+    fontSize: 30,
+    borderWidth: 2,
+  }
+})
